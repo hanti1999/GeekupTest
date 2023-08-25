a7f4dcbe353b4f316445c7dea77afe397d73da13
Hoàng Anh <nth.anh020209@gmail.com>
2023-08-25 08:40:25 +0700

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,77 +3,116 @@ import { Divider, Select, List, Button } from 'antd';
 import { MinusSquareOutlined, CheckCircleOutlined } from '@ant-design/icons/';
 import './home.css';
 
-const Home = () => {
-  // get users
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((json) => {
-        setUser(
-          json.map((user) => ({
-            label: user.name,
-            value: user.id,
-          }))
-        );
-      });
-  }, []);
-  //\\
+const onMakeDone = async (task) => {
+  const resp = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${task.id}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({
+        completed: true,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }
+  );
 
-  // get tasks
+  return resp.json();
+};
+
+const Task = ({ task, onClick }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+
+    await onClick();
+
+    setLoading(false);
+  };
+
+  return (
+    <div className='w-full'>
+      {task.completed === true ? (
+        <CheckCircleOutlined className='completed-icon' />
+      ) : (
+        <MinusSquareOutlined className='undone-icon' />
+      )}
+      {task.title}
+      {task.completed === true ? (
+        ''
+      ) : (
+        <Button
+          size='small'
+          className='markDoneBtn'
+          loading={loading}
+          onClick={handleClick}
+        >
+          Mark done
+        </Button>
+      )}
+    </div>
+  );
+};
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [taskId, setTaskId] = useState(null);
+
+  //User
+  useEffect(() => {
+    const getUsers = async () => {
+      const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+      const json = await resp.json();
+      setUsers(
+        json.map((user) => ({
+          label: user.name,
+          value: user.id,
+        }))
+      );
+    };
+
+    getUsers();
+  }, []);
+
+  //Tasks
+  useEffect(() => {
+    const getTasks = async () => {
+      const resp = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${taskId}/todos`
+      );
+      const json = await resp.json();
+      setTasks(
+        json.map((task) => ({
+          userId: taskId,
+          id: task.id,
+          title: task.title,
+          completed: task.completed,
+        }))
+      );
+    };
+    getTasks();
+  }, [taskId]);
+
   const onChange = (value) => {
     setTaskId(value);
   };
-  const [tasks, setTask] = useState([]);
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${taskId}/todos`)
-      .then((res) => res.json())
-      .then((json) => {
-        setTask(
-          json.map((task) => ({
-            userId: taskId,
-            id: task.id,
-            title: task.title,
-            completed: task.completed,
-          }))
-        );
-      });
-  }, [taskId]);
-  //\\
 
-  // Toggle task status
-  const [loadings, setLoadings] = useState([]);
-  const toggleTaskStatus = (index, task) => {
-    setLoadings((prevLoadings) => {
-      const newLoadings = [...prevLoadings];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
+  const completedTasks = tasks.filter((task) => task.completed === true).length;
 
-    const toggle = (task) => {
-      fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-          completed: true,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-          setLoadings((prevLoadings) => {
-            const newLoadings = [...prevLoadings];
-            newLoadings[index] = false;
-            return newLoadings;
-          });
-        });
-    };
-    toggle(task);
+  const makeDone = async (index, task) => {
+    const resp = await onMakeDone(task);
+
+    if (!resp) return;
+
+    setTasks((prevState) => [
+      ...prevState,
+      (prevState[index].completed = resp.completed),
+    ]);
   };
-  //\\
+
+  const filteredTasks = tasks.sort((a, b) => a.completed - b.completed);
 
   return (
     <div className='container'>
@@ -93,7 +132,7 @@ const Home = () => {
           filterOption={(input, option) =>
             (option?.name ?? '').toLowerCase().includes(input.toLowerCase())
           }
-          options={user}
+          options={users}
         />
       </section>
 
@@ -104,36 +143,22 @@ const Home = () => {
 
         <List
           size='large'
-          style={{ height: '500px', overflow: 'auto' }}
+          className='tasksList'
           bordered
-          dataSource={tasks}
-          renderItem={(task, index) => (
-            <List.Item>
-              {task.completed === true ? (
-                <CheckCircleOutlined className='completed-icon' />
-              ) : (
-                <MinusSquareOutlined className='undone-icon' />
-              )}
-
-              {task.title}
-              {task.completed === true ? (
-                ''
-              ) : (
-                <Button
-                  size='small'
-                  style={{ float: 'right' }}
-                  loading={loadings[index]}
-                  onClick={() => toggleTaskStatus(index, task)}
-                >
-                  Mark done
-                </Button>
-              )}
-            </List.Item>
-          )}
+          dataSource={filteredTasks}
+          renderItem={(task, index) => {
+            return (
+              <List.Item>
+                <Task
+                  task={task}
+                  onClick={async () => await makeDone(index, task)}
+                />
+              </List.Item>
+            );
+          }}
         />
-        <div className=' mt-3'>
-          Done {tasks.filter((task) => task.completed === true).length}/
-          {tasks.map((task) => task.completed).length} Task
+        <div className='mt-3'>
+          Done {completedTasks}/{tasks.length} Task
         </div>
       </section>
     </div>
